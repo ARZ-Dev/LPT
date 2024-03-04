29896711990737e9fabe17d4c08a6bf00cc96e6c
aachour <achour1990@gmail.com>
2024-03-04 09:05:49 +0200

    add till

---
+++ app/Models/Till.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Till extends Model
+{
+    use SoftDeletes;
+    protected $table = 'tills';
+    protected $primaryKey = 'id';
+
+    protected $guarded = [];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class,'user_id');
+    }
+
+    public function fromTransfer()
+    {
+        return $this->hasMany(Transfer::class,'from_till_id');
+    }
+
+    public function toTransfer()
+    {
+        return $this->hasMany(Transfer::class,'to_till_id');
+    }
+
+    public function tillAmount()
+    {
+        return $this->hasMany(TillAmount::class,'till_id');
+    }
+
+    public static function reportMessage($data){
+        echo 'new till <u><a href="'. route('till.view', ['id' => $data['id'], 'status' => 1]).' "> # '.$data['id'].'</a></u>  '.$data['name'].'  has been created ';
+
+    }
+}
