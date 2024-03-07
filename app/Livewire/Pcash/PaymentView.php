68c82ec5c48dccc51a55b44be9a8a243de69b2c0
Karim Jammoul <karimjammoul9@gmail.com>
2024-03-07 15:44:01 +0200

    delete payment function, readding the payment amounts to the linked till amounts

--- app/Livewire/Pcash/PaymentView.php
+++ app/Livewire/Pcash/PaymentView.php
@@ -3,6 +3,7 @@
 namespace App\Livewire\Pcash;
 
 use App\Models\Payment;
+use App\Models\TillAmount;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 use Livewire\Component;
@@ -24,12 +25,26 @@ class PaymentView extends Component
     {
         $this->authorize('payment-delete');
 
-        $payment = Payment::with('paymentAmount')->findOrFail($id);
-        $payment->paymentAmounts()->delete();
+        $payment = Payment::with('paymentAmounts')->findOrFail($id);
+        foreach ($payment->paymentAmounts as $paymentAmount) {
+
+            $tillAmount = TillAmount::where('till_id', $payment->till_id)
+                ->where('currency_id', $paymentAmount->currency_id)
+                ->first();
+
+            if ($tillAmount) {
+                $tillAmount->update([
+                    'amount' => $tillAmount->amount + $paymentAmount->amount,
+                ]);
+            }
+
+
+            $paymentAmount->delete();
+        }
 
         $payment->delete();
 
-        return to_route('payment')->with('success', 'payment has been deleted successfully!');
+        return to_route('payment')->with('success', 'Payment has been deleted successfully!');
     }
 
 
