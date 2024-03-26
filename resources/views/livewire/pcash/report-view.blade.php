a280e9fb2914e86ddecf459aa1b9d28ed4098151
Karim Jammoul <karimjammoul9@gmail.com>
2024-03-26 15:00:59 +0200

    Update report-view.blade.php

--- resources/views/livewire/pcash/report-view.blade.php
+++ resources/views/livewire/pcash/report-view.blade.php
@@ -1,6 +1,31 @@
 <div>
-
     <div class="card">
+        <div class="card-header d-flex justify-content-between">
+            <h4 class="card-title mb-3">Available Tills</h4>
+            @can('till-create')
+            <a href="{{ route('till.create') }}" class="btn btn-primary mb-2 text-nowrap" target="_blank">
+                Create Till
+            </a>
+            @endcan
+        </div>
+        <div class="card-body">
+            <div class="row g-4">
+                @foreach($tills as $till)
+                    <div class="col-2">
+                        <a href="{{ route('till.view', [$till->id, \App\Utils\Constants::VIEW_STATUS]) }}" target="_blank" class="text-decoration-none">
+                            <div class="bg-light rounded-pill p-3 mb-3 text-center">
+                                <h6 class="mb-0">{{ $till->name }}</h6>
+                                <small class="text-muted">{{ $till->user?->full_name }}</small>
+                            </div>
+                        </a>
+                    </div>
+                @endforeach
+
+            </div>
+        </div>
+    </div>
+
+    <div class="card mt-3">
         <div class="card-header d-flex justify-content-between">
             <h4 class="card-title mb-3">Summary</h4>
         </div>
@@ -205,27 +230,6 @@
     </div>
 
     <div class="row mt-1 g-4 mb-4">
-        @can('till-list')
-        <div class="col-sm-6 col-xl-4">
-            <a href="{{ route('till') }}" target="_blank">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="d-flex align-items-start justify-content-between">
-                            <div class="content-left">
-                                <h3>Tills</h3>
-                                <p class="mb-0">Total Tills: {{ number_format($sectionTotalsCount['tills']) }}</p>
-                            </div>
-                            <div class="avatar">
-                              <span class="avatar-initial rounded bg-label-primary">
-                                <i class="ti ti-cash-banknote ti-sm"></i>
-                              </span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </a>
-        </div>
-        @endcan
 
         @can('monthlyEntry-list')
         <div class="col-sm-6 col-xl-4">
