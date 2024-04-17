e2a810bab591df6f75c12e79b606dc102aca1cd5
waelawada10 <136334493+waelawada10@users.noreply.github.com>
2024-04-17 16:06:36 +0300

    like edit in knockoutMap

--- app/Livewire/Tournaments/TournamentCategoryKnockoutMap.php
+++ app/Livewire/Tournaments/TournamentCategoryKnockoutMap.php
@@ -18,18 +18,25 @@ class TournamentCategoryKnockoutMap extends Component
     {
         // $this->category = TournamentLevelCategory::with(['knockoutStages', 'knockoutsMatches.homeTeam','knockoutsMatches.awayTeam','knockoutsMatches.winnerTeam','knockoutsMatches.looserTeam'])->findOrFail($categoryId);
 
-        $this->category = TournamentLevelCategory::with(['knockoutStages','knockoutsMatches' => function ($query) {
-            $query->whereNotNull('home_team_id')->whereNotNull('away_team_id')->whereBetween('knockout_round_id', [1, 4]);
-        }, 'knockoutsMatches.homeTeam','knockoutsMatches.awayTeam','knockoutsMatches.winnerTeam','knockoutsMatches.looserTeam'])->findOrFail($categoryId);
-        // dd($this->category);
+        $this->category = TournamentLevelCategory::with(['knockoutStages', 'knockoutsMatches.knockoutRound' => function ($query) {
+            $query->where('name', 'like', '%Quarter Final%');
+        }, 'knockoutsMatches' => function ($query) {
+            $query->whereNotNull('home_team_id')->whereNotNull('away_team_id');
+        }, 'knockoutsMatches.homeTeam', 'knockoutsMatches.awayTeam', 'knockoutsMatches.winnerTeam', 'knockoutsMatches.loserTeam'])->findOrFail($categoryId);
+        
+        
+        $this->semiFinal = TournamentLevelCategory::with(['knockoutStages', 'knockoutsMatches.knockoutRound' => function ($query) {
+            $query->where('name', 'like', '%Semi Final%');
+        }, 'knockoutsMatches' => function ($query) {
+            $query->whereNotNull('home_team_id')->whereNotNull('away_team_id');
+        }, 'knockoutsMatches.homeTeam', 'knockoutsMatches.awayTeam', 'knockoutsMatches.winnerTeam', 'knockoutsMatches.loserTeam'])->findOrFail($categoryId);
+       
 
-        $this->semiFinal = TournamentLevelCategory::with(['knockoutStages','knockoutsMatches' => function ($query) {
-            $query->whereNotNull('home_team_id')->whereNotNull('away_team_id')->whereBetween('knockout_round_id', [5, 6]);
-        }, 'knockoutsMatches.homeTeam','knockoutsMatches.awayTeam','knockoutsMatches.winnerTeam','knockoutsMatches.looserTeam'])->findOrFail($categoryId);
-
-        $this->final = TournamentLevelCategory::with(['knockoutStages','knockoutsMatches' => function ($query) {
-            $query->whereNotNull('home_team_id')->whereNotNull('away_team_id')->where('knockout_round_id', 7);
-        }, 'knockoutsMatches.homeTeam','knockoutsMatches.awayTeam','knockoutsMatches.winnerTeam','knockoutsMatches.looserTeam'])->findOrFail($categoryId);
+        $this->final = TournamentLevelCategory::with(['knockoutStages', 'knockoutsMatches.knockoutRound' => function ($query) {
+            $query->where('name', 'like', '%Final%');
+        }, 'knockoutsMatches' => function ($query) {
+            $query->whereNotNull('home_team_id')->whereNotNull('away_team_id');
+        }, 'knockoutsMatches.homeTeam', 'knockoutsMatches.awayTeam', 'knockoutsMatches.winnerTeam', 'knockoutsMatches.loserTeam'])->findOrFail($categoryId);
    
         // dd($this->semiFinal);
     }
