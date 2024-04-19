d6501bbd6ed468ed4bcb55b05d8879e74eb6a557
waelawada10 <136334493+waelawada10@users.noreply.github.com>
2024-04-19 14:57:57 +0300

    KnockoutStageView bug solved

--- app/Livewire/Matches/KnockoutStageView.php
+++ app/Livewire/Matches/KnockoutStageView.php
@@ -16,9 +16,10 @@ class KnockoutStageView extends Component
    
     public function mount ($id){
 
-        $this->knockoutStages =KnockoutStage::with('knockoutRounds','tournamentDeuceType','games')->get();
         $this->tournament = Tournament::findOrFail($id);
-
+        $this->knockoutStages = KnockoutStage::with('knockoutRounds', 'tournamentDeuceType', 'games', 'tournamentLevelCategory')->whereHas('tournamentLevelCategory', function ($query) {
+        $query->where('tournament_id', $this->tournament->id);
+    })->get();
 
     }
 
