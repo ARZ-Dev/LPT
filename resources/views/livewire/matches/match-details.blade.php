e479f6469fb1c2a5a27471b8c510033265b1e636
Karim Jammoul <karimjammoul9@gmail.com>
2024-04-30 10:56:23 +0300

    Update match-details.blade.php

--- resources/views/livewire/matches/match-details.blade.php
+++ resources/views/livewire/matches/match-details.blade.php
@@ -9,36 +9,36 @@
     </h4>
 
     <div class="row overflow-hidden">
-                <div class="col-12">
-                    <ul class="timeline timeline-center mt-5">
-                    @foreach($points as $point)
-                        <li class="timeline-item timeline-item-{{ $point->point_team_id == $match->home_team_id ? "left" : "right" }} mt-1">
+        <div class="col-12">
+            <ul class="timeline timeline-center mt-5">
+            @foreach($points as $point)
+                <li class="timeline-item timeline-item-{{ $point->point_team_id == $match->home_team_id ? "left" : "right" }} mt-1">
 
-                        <span class="timeline-indicator timeline-indicator-primary" data-aos="zoom-in" data-aos-delay="200">
-                          <i class="ti ti-ball-tennis ti-sm"></i>
-                        </span>
-                            <div class="timeline-event card p-0" data-aos="fade-left">
-                                <div class="card-header d-flex justify-content-between align-items-center flex-wrap">
-                                    <h6 class="card-title mb-0">
-                                        {{ $point->point_team_id == $match->home_team_id ? $match->homeTeam->nickname : $match->awayTeam->nickname }}
-                                    </h6>
+                <span class="timeline-indicator timeline-indicator-primary" data-aos="zoom-in" data-aos-delay="200">
+                  <i class="ti ti-ball-tennis ti-sm"></i>
+                </span>
+                    <div class="timeline-event card p-0" data-aos="fade-left">
+                        <div class="card-header d-flex justify-content-between align-items-center flex-wrap">
+                            <h6 class="card-title mb-0">
+                                {{ $point->point_team_id == $match->home_team_id ? $match->homeTeam->nickname : $match->awayTeam->nickname }}
+                            </h6>
+                        </div>
+                        <div class="card-body">
+                            <p class="mb-2"> {{$point->home_team_score}} - {{$point->away_team_score}}</p>
+                            <div class="d-flex justify-content-between align-items-center flex-wrap">
+                                <div>
+                                    <p class="text-muted mb-2">Point Nb : {{$point->point_number}}</p>
+                                    <ul class="list-unstyled users-list d-flex align-items-center avatar-group"></ul>
                                 </div>
-                                <div class="card-body">
-                                    <p class="mb-2"> {{$point->home_team_score}} - {{$point->away_team_score}}</p>
-                                    <div class="d-flex justify-content-between align-items-center flex-wrap">
-                                        <div>
-                                            <p class="text-muted mb-2">Point Nb : {{$point->point_number}}</p>
-                                            <ul class="list-unstyled users-list d-flex align-items-center avatar-group"></ul>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="timeline-event-time" style="font-size: 12px;">{{ \Carbon\Carbon::parse($point->created_at)->format('jS F H:i') }}</div>
                             </div>
-                      </li>
-                    @endforeach
-                    </ul>
-                </div>
-            </div>
+                        </div>
+                        <div class="timeline-event-time" style="font-size: 12px;">{{ \Carbon\Carbon::parse($point->created_at)->format('jS F H:i:s') }}</div>
+                    </div>
+              </li>
+            @endforeach
+            </ul>
+        </div>
+    </div>
 </div>
 
 
