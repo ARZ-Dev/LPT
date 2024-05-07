f5b5f6e5fc1cbb0ac97aeb2dfc79b6194478d464
Karim Jammoul <karimjammoul9@gmail.com>
2024-05-07 16:51:12 +0300

    changing tournament categories ui

--- resources/views/livewire/tournaments/tournament-category-form.blade.php
+++ resources/views/livewire/tournaments/tournament-category-form.blade.php
@@ -1,60 +1,106 @@
 <div>
     <form>
 
-        <div class="card mb-2">
-            <div class="card-header d-flex justify-content-between align-items-center">
-                <h5 class="mb-0">
-                    {{ $tournamentLevelCategory?->levelCategory?->name }} Info
-                </h5>
-                <div>
-                    <a href="{{ route('tournaments-categories', $tournament->id) }}"
-                       class="btn btn-primary mb-2 text-nowrap"
-                    >
-                        {{ $tournament->name }} Categories
-                    </a>
-                </div>
-            </div>
-            <div class="card-body">
-                <div class="row g-3">
-                    <div class="col-12 col-sm-6">
-                        <label class="form-label">Start Date *</label>
-                        <input wire:model="start_date" type="date" class="form-control" min="{{ $tournament->start_date }}" max="{{ $tournament->end_date }}">
-                        @error('start_date') <div class="text-danger">{{ $message }}</div> @enderror
-                    </div>
-                    <div class="col-12 col-sm-6">
-                        <label class="form-label">End Date *</label>
-                        <input wire:model="end_date" type="date" class="form-control" min="{{ $tournament->start_date }}" max="{{ $tournament->end_date }}">
-                        @error('end_date') <div class="text-danger">{{ $message }}</div> @enderror
-                    </div>
-                    <div class="col-12 col-md-6">
-                        <label class="form-label" for="tournament-type">Type *</label>
-                        <div wire:ignore>
-                            <select
-                                wire:model="type_id"
-                                id="tournament-type"
-                                class="selectpicker w-100"
-                                title="Select Type"
-                                data-style="btn-default"
-                                data-live-search="true"
-                                data-icon-base="ti"
-                                data-size="5"
-                                data-tick-icon="ti-check text-white" required>
-                                @foreach($tournamentTypes as $tournamentType)
-                                    <option value="{{ $tournamentType->id }}" @selected(($type_id ?? null) == $tournamentType->id)>{{ $tournamentType->name }}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                        @error('type_id') <div class="text-danger">{{ $message }}</div> @enderror
-                    </div>
-                    <div class="col-12 col-sm-6">
-                        <label for="nb-of-teams" class="form-label">Number of Teams:</label>
-                        <input wire:model="nb_of_teams" id="nb-of-teams" type="number" class="form-control dt-input" placeholder="Number of Teams" disabled readonly>
-                        @error('nb_of_teams') <div class="text-danger">{{ $message }}</div> @enderror
-                    </div>
-                    <div class="col-12 col-sm-12 mt-4">
-                        <label class="switch switch-primary">
-                            <input wire:model.live="has_group_stages" @checked($has_group_stages ?? false) type="checkbox" class="switch-input" />
-                            <span class="switch-toggle-slider">
+        <div class="col-xl-12">
+            <div class="nav-align-top mb-4">
+                <ul class="nav nav-pills mb-3 nav-fill" role="tablist">
+                    <li class="nav-item">
+                        <button
+                            type="button"
+                            class="nav-link active"
+                            role="tab"
+                            data-bs-toggle="tab"
+                            data-bs-target="#navs-pills-justified-home"
+                            aria-controls="navs-pills-justified-home"
+                            aria-selected="true">
+                            <i class="tf-icons ti ti-home ti-xs me-1"></i>
+                            {{ $tournamentLevelCategory->levelCategory?->name }} Details
+                        </button>
+                    </li>
+                    <li class="nav-item">
+                        <button
+                            type="button"
+                            class="nav-link"
+                            role="tab"
+                            data-bs-toggle="tab"
+                            data-bs-target="#navs-pills-justified-messages"
+                            aria-controls="navs-pills-justified-messages"
+                            aria-selected="false">
+                            <i class="tf-icons ti ti-settings ti-xs me-1"></i>
+                            Stages Settings
+                        </button>
+                    </li>
+                    <li class="nav-item">
+                        <button
+                            type="button"
+                            class="nav-link"
+                            role="tab"
+                            data-bs-toggle="tab"
+                            data-bs-target="#navs-pills-justified-messages"
+                            aria-controls="navs-pills-justified-messages"
+                            aria-selected="false">
+                            <i class="tf-icons ti ti-tournament ti-xs me-1"></i>
+                            Knockout Map
+                        </button>
+                    </li>
+                    <li class="nav-item">
+                        <button
+                            type="button"
+                            class="nav-link"
+                            role="tab"
+                            data-bs-toggle="tab"
+                            data-bs-target="#navs-pills-justified-messages"
+                            aria-controls="navs-pills-justified-messages"
+                            aria-selected="false">
+                            <i class="tf-icons ti ti-list-numbers ti-xs me-1"></i>
+                            Matches
+                        </button>
+                    </li>
+                </ul>
+                <div class="tab-content">
+                    <div class="tab-pane fade show active" id="navs-pills-justified-home" role="tabpanel">
+
+
+                        <div class="row g-3">
+                            <div class="col-12 col-sm-6">
+                                <label class="form-label">Start Date *</label>
+                                <input wire:model="start_date" type="date" class="form-control" min="{{ $tournament->start_date }}" max="{{ $tournament->end_date }}">
+                                @error('start_date') <div class="text-danger">{{ $message }}</div> @enderror
+                            </div>
+                            <div class="col-12 col-sm-6">
+                                <label class="form-label">End Date *</label>
+                                <input wire:model="end_date" type="date" class="form-control" min="{{ $tournament->start_date }}" max="{{ $tournament->end_date }}">
+                                @error('end_date') <div class="text-danger">{{ $message }}</div> @enderror
+                            </div>
+                            <div class="col-12 col-md-6">
+                                <label class="form-label" for="tournament-type">Type *</label>
+                                <div wire:ignore>
+                                    <select
+                                        wire:model="type_id"
+                                        id="tournament-type"
+                                        class="selectpicker w-100"
+                                        title="Select Type"
+                                        data-style="btn-default"
+                                        data-live-search="true"
+                                        data-icon-base="ti"
+                                        data-size="5"
+                                        data-tick-icon="ti-check text-white" required>
+                                        @foreach($tournamentTypes as $tournamentType)
+                                            <option value="{{ $tournamentType->id }}" @selected(($type_id ?? null) == $tournamentType->id)>{{ $tournamentType->name }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                                @error('type_id') <div class="text-danger">{{ $message }}</div> @enderror
+                            </div>
+                            <div class="col-12 col-sm-6">
+                                <label for="nb-of-teams" class="form-label">Number of Teams:</label>
+                                <input wire:model="nb_of_teams" id="nb-of-teams" type="number" class="form-control dt-input" placeholder="Number of Teams" disabled readonly>
+                                @error('nb_of_teams') <div class="text-danger">{{ $message }}</div> @enderror
+                            </div>
+                            <div class="col-12 col-sm-12 mt-4">
+                                <label class="switch switch-primary">
+                                    <input wire:model.live="has_group_stages" @checked($has_group_stages ?? false) type="checkbox" class="switch-input" />
+                                    <span class="switch-toggle-slider">
                                 <span class="switch-on">
                                   <i class="ti ti-check"></i>
                                 </span>
@@ -62,60 +108,71 @@
                                   <i class="ti ti-x"></i>
                                 </span>
                             </span>
-                            <span class="switch-label">Has Group Stages?</span>
-                        </label>
-                    </div>
-                    <div class="col-md-6 col-sm-12 {{ ($has_group_stages ?? false) ? "" : "d-none" }}">
-                        <label for="nb-of-teams" class="form-label">Number of Groups:</label>
-                        <input wire:model="nb_of_groups" id="nb-of-groups" type="number" class="form-control dt-input" placeholder="Number of Groups">
-                        @error('nb_of_groups') <div class="text-danger">{{ $message }}</div> @enderror
-                    </div>
-                    <div class="col-md-6 col-sm-12 {{ ($has_group_stages ?? false) ? "" : "d-none" }}">
-                        <label for="nb-of-teams" class="form-label">Number of Winners per Group:</label>
-                        <input wire:model="nb_of_winners_per_group" id="nb-of-winners" type="number" class="form-control dt-input" placeholder="Number of Winners per Group">
-                        @error('nb_of_winners_per_group') <div class="text-danger">{{ $message }}</div> @enderror
-                        @error('knockout_teams') <div class="text-danger">{{ $message }}</div> @enderror
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="card mt-4 mb-2">
-            <div class="card-header d-flex justify-content-between">
-                <h5 class="mb-0">
-                    {{ $tournamentLevelCategory?->levelCategory?->name }} {{ $tournament->is_free ? "" : "Paid" }} Teams
-                    @error('selectedTeamsIds') <div class="text-danger">{{ $message }}</div> @enderror
-                </h5>
-                <div class="col-2 mb-2">
-                    <input wire:model.live="teams_filter_search" type="text" placeholder="Search..." class="form-control" />
-                </div>
-            </div>
-            <div class="card-body">
-                <div class="row g-3">
-                    <div class="col-12 col-sm-12">
-                        <div class="row overflow-auto h-px-400">
-                            @forelse($teams as $team)
-                                <div class="col-md-3 my-2">
-                                    <div class="card bg-label-{{ in_array($team->id, $selectedTeamsIds) ? "linkedin" : "danger" }}">
-                                        <div class="card-body">
-                                            <div class="d-flex justify-content-between">
-                                                <h5 class="card-title">{{ $team->nickname }}</h5>
-                                                <a wire:click="toggleTeam({{ $team->id }})" class="btn rounded-pill btn-{{ in_array($team->id, $selectedTeamsIds) ? "warning" : "success" }} text-nowrap text-white">
-                                                    {{ in_array($team->id, $selectedTeamsIds) ? "Remove" : "Select" }}
-                                                </a>
-                                            </div>
-                                            <p class="card-text">Players: {{ count($team->players) }}</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            @empty
-                                <div class="col-md-12">
-                                    <div class="alert alert-danger" role="alert">
-                                        No teams available.
-                                    </div>
-                                </div>
-                            @endforelse
+                                    <span class="switch-label">Has Group Stages?</span>
+                                </label>
+                            </div>
+                            <div class="col-md-6 col-sm-12 {{ ($has_group_stages ?? false) ? "" : "d-none" }}">
+                                <label for="nb-of-teams" class="form-label">Number of Groups:</label>
+                                <input wire:model="nb_of_groups" id="nb-of-groups" type="number" class="form-control dt-input" placeholder="Number of Groups">
+                                @error('nb_of_groups') <div class="text-danger">{{ $message }}</div> @enderror
+                            </div>
+                            <div class="col-md-6 col-sm-12 {{ ($has_group_stages ?? false) ? "" : "d-none" }}">
+                                <label for="nb-of-teams" class="form-label">Number of Winners per Group:</label>
+                                <input wire:model="nb_of_winners_per_group" id="nb-of-winners" type="number" class="form-control dt-input" placeholder="Number of Winners per Group">
+                                @error('nb_of_winners_per_group') <div class="text-danger">{{ $message }}</div> @enderror
+                                @error('knockout_teams') <div class="text-danger">{{ $message }}</div> @enderror
+                            </div>
                         </div>
+
+                        <div class="row g-3 mt-2">
+                            <div class="d-flex justify-content-end">
+                                <div class="col-2 mb-2">
+                                    <input wire:model.live="teams_filter_search" type="text" placeholder="Search..." class="form-control" />
+                                </div>
+                            </div>
+                            <div class="col-12 col-sm-12">
+                                <div class="row overflow-auto h-px-400">
+                                    @forelse($teams as $team)
+                                        <div class="col-md-3 my-2">
+                                            <div class="card bg-label-{{ in_array($team->id, $selectedTeamsIds) ? "linkedin" : "danger" }}">
+                                                <div class="card-body">
+                                                    <div class="d-flex justify-content-between">
+                                                        <h5 class="card-title">{{ $team->nickname }}</h5>
+                                                        <a wire:click="toggleTeam({{ $team->id }})" class="btn rounded-pill btn-{{ in_array($team->id, $selectedTeamsIds) ? "warning" : "success" }} text-nowrap text-white">
+                                                            {{ in_array($team->id, $selectedTeamsIds) ? "Remove" : "Select" }}
+                                                        </a>
+                                                    </div>
+                                                    <p class="card-text">Players: {{ count($team->players) }}</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    @empty
+                                        <div class="col-md-12">
+                                            <div class="alert alert-danger" role="alert">
+                                                No teams available.
+                                            </div>
+                                        </div>
+                                    @endforelse
+                                </div>
+                            </div>
+                        </div>
+
+
+                    </div>
+                    <div class="tab-pane fade" id="navs-pills-justified-profile" role="tabpanel">
+
+
+                    </div>
+                    <div class="tab-pane fade" id="navs-pills-justified-messages" role="tabpanel">
+                        <p>
+                            Oat cake chupa chups dragée donut toffee. Sweet cotton candy jelly beans macaroon gummies
+                            cupcake gummi bears cake chocolate.
+                        </p>
+                        <p class="mb-0">
+                            Cake chocolate bar cotton candy apple pie tootsie roll ice cream apple pie brownie cake. Sweet
+                            roll icing sesame snaps caramels danish toffee. Brownie biscuit dessert dessert. Pudding jelly
+                            jelly-o tart brownie jelly.
+                        </p>
                     </div>
                 </div>
             </div>
