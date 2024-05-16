84e344a14556f4b89dec05ab75290e2c29ad0200
Karim Jammoul <karimjammoul9@gmail.com>
2024-05-17 00:31:14 +0300

    optimizing player page code and removing unused files

--- app/Livewire/Players/PlayerForm.php
+++ app/Livewire/Players/PlayerForm.php
@@ -2,13 +2,13 @@
 
 namespace App\Livewire\Players;
 
-use App\Livewire\Forms\PlayerFormData;
 use App\Models\Country;
 use App\Models\Player;
+use App\Models\PlayerTeam;
 use App\Models\Team;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Storage;
+use Illuminate\Validation\Rules\Exists;
 use Livewire\Attributes\On;
 use Livewire\Component;
 use Livewire\WithFileUploads;
@@ -22,13 +22,25 @@ class PlayerForm extends Component
     public bool $editing = false;
     public int $status = 0;
     public $teams = [];
-    public PlayerFormData $form;
     public $countries = [];
     public $nationalIdFile;
     public $player;
     public bool $submitting = false;
     public array $teamPlayersIds = [];
     public $playerTeams = [];
+
+    // form fields - start
+    public $first_name = '';
+    public $middle_name = '';
+    public $last_name = '';
+    public $current_team_id = null;
+    public $birthdate = '';
+    public $email = '';
+    public $phone_number = '';
+    public $country_id = '';
+    public $nickname = '';
+    public $playing_side = '';
+    // form fields - end
 
     public function mount($id = 0, $status = 0)
     {
@@ -46,7 +58,7 @@ class PlayerForm extends Component
 
             $this->player = Player::with(['teams' => ['levelCategory', 'players']])->findOrFail($id);
             $this->editing = true;
-            $this->form->setPlayer($this->player);
+            $this->setPlayerData($this->player);
             $this->teamPlayersIds = $this->player->currentTeam?->players->pluck('id')->toArray() ?? [];
             $this->playerTeams = $this->player->teams;
 
@@ -56,16 +68,44 @@ class PlayerForm extends Component
 
     }
 
+    public function setPlayerData($player)
+    {
+        $this->player = $player;
+        $this->first_name = $player->first_name;
+        $this->middle_name = $player->middle_name;
+        $this->last_name = $player->last_name;
+        $this->current_team_id = $player->current_team_id;
+        $this->birthdate = $player->birthdate;
+        $this->email = $player->email;
+        $this->phone_number = $player->phone_number;
+        $this->country_id = $player->country_id;
+        $this->nickname = $player->nickname;
+        $this->playing_side = $player->playing_side;
+    }
+
+    public function rules()
+    {
+        return [
+            'first_name' => ['required', 'max:255'],
+            'middle_name' => ['required', 'max:255'],
+            'last_name' => ['required', 'max:255'],
+            'current_team_id' => ['nullable', new Exists('teams', 'id')],
+            'birthdate' => ['required', 'date'],
+            'email' => ['nullable', 'email'],
+            'phone_number' => ['required'],
+            'country_id' => ['required'],
+            'nickname' => ['required'],
+            'playing_side' => ['required', 'in:left,right'],
+            'nationalIdFile' => ['nullable', 'file', 'max:2048']
+        ];
+    }
+
     public function store()
     {
         $this->validate();
 
-        $this->validate([
-            'nationalIdFile' => ['nullable', 'file', 'max:2048']
-        ]);
-
         if($this->submitting) {
-            return;
+            return false;
         }
         $this->submitting = true;
 
@@ -77,9 +117,35 @@ class PlayerForm extends Component
                 $path = $this->nationalIdFile->storePublicly(path: 'public/national_ids');
             }
 
-            $this->form->store($path);
+            $data = [
+                'first_name' => $this->first_name,
+                'middle_name' => $this->middle_name,
+                'last_name' => $this->last_name,
+                'current_team_id' => $this->current_team_id,
+                'birthdate' => $this->birthdate,
+                'email' => $this->email,
+                'phone_number' => $this->phone_number,
+                'country_id' => $this->country_id,
+                'nickname' => $this->nickname,
+                'playing_side' => $this->playing_side,
+            ];
 
-            $team = Team::findOrFail($this->form->current_team_id);
+            if ($path) {
+                $data['national_id_upload'] = $path;
+            }
+
+            $player = Player::create($data);
+
+            if ($this->current_team_id) {
+                PlayerTeam::updateOrCreate([
+                    'player_id' => $player->id,
+                    'team_id' => $this->current_team_id,
+                ],[
+                    'playing_side' => $this->playing_side,
+                ]);
+            }
+
+            $team = Team::findOrFail($this->current_team_id);
             $teamPlayers = $team->players()->count();
             throw_if($teamPlayers > 2, new \Exception($team->nickname . ' has no available spots for new players!'));
 
@@ -94,7 +160,6 @@ class PlayerForm extends Component
             ]);
         }
 
-
         return to_route('players')->with('success', 'Player has been added successfully!');
     }
 
@@ -102,12 +167,8 @@ class PlayerForm extends Component
     {
         $this->validate();
 
-        $this->validate([
-            'nationalIdFile' => ['nullable', 'file', 'max:2048']
-        ]);
-
         if($this->submitting) {
-            return;
+            return false;
         }
         $this->submitting = true;
 
@@ -119,13 +180,39 @@ class PlayerForm extends Component
                 $path = $this->nationalIdFile->storePublicly(path: 'public/national_ids');
             }
 
-            $this->form->update($path);
+            $data = [
+                'first_name' => $this->first_name,
+                'middle_name' => $this->middle_name,
+                'last_name' => $this->last_name,
+                'current_team_id' => $this->current_team_id,
+                'birthdate' => $this->birthdate,
+                'email' => $this->email,
+                'phone_number' => $this->phone_number,
+                'country_id' => $this->country_id,
+                'nickname' => $this->nickname,
+                'playing_side' => $this->playing_side,
+            ];
 
-            $team = Team::findOrFail($this->form->current_team_id);
-            $teamPlayers = $team->players()->count();
+            if ($path) {
+                $data['national_id_upload'] = $path;
+            }
 
-            if (!in_array($this->player->id, $this->teamPlayersIds)) {
-                throw_if($teamPlayers > 2, new \Exception($team->nickname . ' has no available spots for new players!'));
+            $this->player->update($data);
+
+            if ($this->current_team_id) {
+                PlayerTeam::updateOrCreate([
+                    'player_id' => $this->player->id,
+                    'team_id' => $this->current_team_id,
+                ],[
+                    'playing_side' => $this->playing_side,
+                ]);
+
+                $team = Team::findOrFail($this->current_team_id);
+                $teamPlayers = $team->players()->count();
+
+                if (!in_array($this->player->id, $this->teamPlayersIds)) {
+                    throw_if($teamPlayers > 2, new \Exception($team->nickname . ' has no available spots for new players!'));
+                }
             }
 
             DB::commit();
--- resources/views/livewire/players/player-form.blade.php
+++ resources/views/livewire/players/player-form.blade.php
@@ -16,80 +16,80 @@
                             <div class="col-12 col-md-4">
                                 <label class="form-label" for="first_name">First Name *</label>
                                 <input
-                                    wire:model="form.first_name"
+                                    wire:model="first_name"
                                     type="text"
                                     id="first_name"
                                     name="first_name"
                                     class="form-control"
                                     placeholder="First Name"
                                 />
-                                @error('form.first_name') <div class="text-danger">{{ $message }}</div> @enderror
+                                @error('first_name') <div class="text-danger">{{ $message }}</div> @enderror
                             </div>
                             <div class="col-12 col-md-4">
                                 <label class="form-label" for="middle_name">Middle Name *</label>
                                 <input
-                                    wire:model="form.middle_name"
+                                    wire:model="middle_name"
                                     type="text"
                                     id="middle_name"
                                     name="middle_name"
                                     class="form-control"
                                     placeholder="Middle Name"
                                 />
-                                @error('form.middle_name') <div class="text-danger">{{ $message }}</div> @enderror
+                                @error('middle_name') <div class="text-danger">{{ $message }}</div> @enderror
                             </div>
                             <div class="col-12 col-md-4">
                                 <label class="form-label" for="last_name">Last Name *</label>
                                 <input
-                                    wire:model="form.last_name"
+                                    wire:model="last_name"
                                     type="text"
                                     id="last_name"
                                     name="last_name"
                                     class="form-control"
                                     placeholder="Last Name"
                                 />
-                                @error('form.last_name') <div class="text-danger">{{ $message }}</div> @enderror
+                                @error('last_name') <div class="text-danger">{{ $message }}</div> @enderror
                             </div>
                             <div class="col-12 col-md-6">
                                 <label class="form-label" for="birthdate">Birthdate *</label>
                                 <input
-                                    wire:model="form.birthdate"
+                                    wire:model="birthdate"
                                     type="date"
                                     id="birthdate"
                                     name="birthdate"
                                     class="form-control"
                                     max="{{ now()->format('Y-m-d') }}"
                                 />
-                                @error('form.birthdate') <div class="text-danger">{{ $message }}</div> @enderror
+                                @error('birthdate') <div class="text-danger">{{ $message }}</div> @enderror
                             </div>
                             <div class="col-12 col-md-6">
                                 <label class="form-label" for="email">Email</label>
                                 <input
-                                    wire:model="form.email"
+                                    wire:model="email"
                                     type="email"
                                     id="email"
                                     name="email"
                                     class="form-control"
                                     placeholder="Email"
                                 />
-                                @error('form.email') <div class="text-danger">{{ $message }}</div> @enderror
+                                @error('email') <div class="text-danger">{{ $message }}</div> @enderror
                             </div>
                             <div class="col-12 col-md-6">
                                 <label class="form-label" for="phone_number">Phone Number *</label>
                                 <input
-                                    wire:model="form.phone_number"
+                                    wire:model="phone_number"
                                     type="text"
                                     id="phone_number"
                                     name="phone_number"
                                     class="form-control"
                                     placeholder="Phone Number"
                                 />
-                                @error('form.phone_number') <div class="text-danger">{{ $message }}</div> @enderror
+                                @error('phone_number') <div class="text-danger">{{ $message }}</div> @enderror
                             </div>
                             <div class="col-12 col-md-6">
                                 <label class="form-label" for="country_id">Country *</label>
                                 <div wire:ignore>
                                     <select
-                                        wire:model="form.country_id"
+                                        wire:model="country_id"
                                         id="country_id"
                                         class="selectpicker w-100"
                                         title="Select Country"
@@ -99,24 +99,24 @@
                                         data-size="5"
                                         data-tick-icon="ti-check text-white" required>
                                         @foreach($countries as $country)
-                                            <option value="{{ $country->id }}" @selected($country->id == $form->country_id)>{{ $country->name }}</option>
+                                            <option value="{{ $country->id }}" @selected($country->id == $country_id)>{{ $country->name }}</option>
                                         @endforeach
                                     </select>
                                 </div>
-                                @error('form.country_id') <div class="text-danger">{{ $message }}</div> @enderror
+                                @error('country_id') <div class="text-danger">{{ $message }}</div> @enderror
                             </div>
 
                             <div class="col-12 col-md-6">
                                 <label class="form-label" for="nickname">Nickname *</label>
                                 <input
-                                    wire:model="form.nickname"
+                                    wire:model="nickname"
                                     type="text"
                                     id="nickname"
                                     name="nickname"
                                     class="form-control"
                                     placeholder="Nickname"
                                 />
-                                @error('form.nickname') <div class="text-danger">{{ $message }}</div> @enderror
+                                @error('nickname') <div class="text-danger">{{ $message }}</div> @enderror
                             </div>
                         </div>
                     </div>
@@ -132,26 +132,27 @@
                                 <label class="form-label" for="team_id">Current Team</label>
                                 <div wire:ignore>
                                     <select
-                                        wire:model="form.current_team_id"
+                                        wire:model="current_team_id"
                                         id="team_id"
                                         class="selectpicker w-100 @error('team_id') invalid-validation-select @enderror"
                                         title="Select Team"
                                         data-style="btn-default"
                                         data-live-search="true"
                                         data-icon-base="ti"
+                                        data-size="5"
                                         data-tick-icon="ti-check text-white" required>
                                         @foreach($teams as $team)
-                                            <option value="{{ $team->id }}" @selected($team->id == $form->current_team_id)>{{ $team->nickname }}</option>
+                                            <option value="{{ $team->id }}" @selected($team->id == $current_team_id)>{{ $team->nickname }}</option>
                                         @endforeach
                                     </select>
                                 </div>
-                                @error('form.team_id') <div class="text-danger">{{ $message }}</div> @enderror
+                                @error('team_id') <div class="text-danger">{{ $message }}</div> @enderror
                             </div>
                             <div class="col-12 col-md-6">
                                 <label class="form-label" for="playing_side">Playing Side *</label>
                                 <div wire:ignore>
                                     <select
-                                        wire:model="form.playing_side"
+                                        wire:model="playing_side"
                                         id="playing_side"
                                         class="selectpicker w-100 @error('playing_side') invalid-validation-select @enderror"
                                         title="Select Playing Side"
@@ -159,11 +160,11 @@
                                         data-live-search="true"
                                         data-icon-base="ti"
                                         data-tick-icon="ti-check text-white" required>
-                                        <option value="left" @selected("left" == $form->playing_side)>Left</option>
-                                        <option value="right" @selected("right" == $form->playing_side)>Right</option>
+                                        <option value="left" @selected("left" == $playing_side)>Left</option>
+                                        <option value="right" @selected("right" == $playing_side)>Right</option>
                                     </select>
                                 </div>
-                                @error('form.playing_side') <div class="text-danger">{{ $message }}</div> @enderror
+                                @error('playing_side') <div class="text-danger">{{ $message }}</div> @enderror
                             </div>
 
                             @if(count($playerTeams))
@@ -219,17 +220,6 @@
 
     @script
     <script>
-
-
-        document.addEventListener('livewire:navigated', function () {
-
-            $(document).ready(function () {
-
-
-
-            });
-
-        })
 
     </script>
     @endscript
